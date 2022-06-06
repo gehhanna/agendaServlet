ce032600a1d765a9979e4b0d0a0e4c207693c900
gehhanna <ge.hanna2015@gmail.com>
2022-06-06 11:59:31 -0300

    Teste de conexao com o banco
    Teste de conexao com o banco

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,39 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	
+	/** Módulo de conexão **/
+	// Parâmetros de conexão
+	private String driver = "com.mysql.cj.jdbc.driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	
+	private String user = "root";
+	private String password = "";
+
+	// Mêtodo de conexão.
+	
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println("Erro ao conectar ao banco\n" + e);
+			return null;
+		}
+	}
+	// teste de coneão
+	public void testeConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
--- src/main/java/model/JavaBeans.java
+++ src/main/java/model/JavaBeans.java
@@ -9,6 +9,18 @@ public class JavaBeans {
 	
 	
 	
+	public JavaBeans() {
+		super();
+	}
+	
+	public JavaBeans(String idcon, String nome, String fone, String email) {
+		super();
+		this.idcon = idcon;
+		this.nome = nome;
+		this.fone = fone;
+		this.email = email;
+	}
+
 	public String getIdcon() {
 		return idcon;
 	}
