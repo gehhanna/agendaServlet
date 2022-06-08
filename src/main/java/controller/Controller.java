d0ff2134f3f3ef09f60762dbef8ec2bcd827cc7e
gehhanna <ge.hanna2015@gmail.com>
2022-06-08 12:40:24 -0300

    Criando o validador para o botao de cadastro

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,14 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
 /**
  * Servlet implementation class Controller
  */
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -27,6 +29,10 @@ public class Controller extends HttpServlet {
 		System.out.println(action);
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 
@@ -35,5 +41,17 @@ public class Controller extends HttpServlet {
 			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
 
+	}
+	// Novoc contato.
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// teste de recebimento dos dados do formulário.
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("fone"));
+		System.out.println(request.getParameter("email"));
+		// setar as variáveis do JavaBeans
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
 	}
 }
--- src/main/webapp/scripts/validador.js
+++ src/main/webapp/scripts/validador.js
@@ -1,3 +1,23 @@
 /**
- * 
+ * Validação de formúlario
+  Author George Hanna
  */
+ 
+ function validar(){
+	let nome = frmContato.nome.value
+	let fone = frmContato.fone.value
+	if (nome == ""){
+		alert('Preencha o campo Nome')
+		frmContato.nome.focus()
+		return false
+	} else if (fone == ""){
+		alert('Preencha o campo fone')
+		frmContato.fone.focus()
+		return false
+	} else {
+		document.forms["frmContato"].submit()
+		}	
+	}
+	
+	
+ 
